487050cb51ba657b175012aa9445eeb68b368117
Santiago Lorenzatti <slorenzatti@facultad.sanfrancisco.utn.edu.ar>
2025-08-26 17:32:57 -0300

    Implementacion de Cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import sequelize from "./models/index.js";
 import taskRoutes from "./routes/tasks.js";
 import userRoutes from "./routes/users.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json()); // Para leer JSON en el body
@@ -10,6 +11,7 @@ app.use(express.json()); // Para leer JSON en el body
 // Rutas
 app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes);
+app.use(cors());
 
 const PORT = 3000;
 
